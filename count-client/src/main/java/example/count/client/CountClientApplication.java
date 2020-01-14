f680dce25c14cb6cf23e4ff2720d75f64ae4ad00
gregwhitaker <gwhitake@gmail.com>
2020-01-13 20:56:13 -0800

    Initial Commit

---
+++ count-client/src/main/java/example/count/client/CountClientApplication.java
@@ -0,0 +1,11 @@
+package example.count.client;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountClientApplication {
+
+    public static void main(String... args) {
+
+    }
+}
